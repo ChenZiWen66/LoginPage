44f07945284c73d9977d812c77de948262b92696
chenziwen <401030526@qq.com>
2021-01-14 22:27:51 +0800

    1.添加了一些注释，描述了Vue3有哪些不同的地方

--- login_page/src/views/Home.vue
+++ login_page/src/views/Home.vue
@@ -1,17 +1,25 @@
 <template>
+  <!-- Vue3中不需要唯一的跟标签，vue2需要 -->
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
+    <!--  使用一个子级组件  -->
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
 </template>
 
 <script lang="ts">
+// defineComponent函数，目的是定义一个组件，内部可以传入一个配置对象
 import { defineComponent } from 'vue'
+// 引入了一个子级组件
 import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
 
+// 暴露出定义好的组件
 export default defineComponent({
+  // 当前组件的名字
   name: 'Home',
+  //  注册组件
   components: {
+    // 注册一个子级组件
     HelloWorld
   }
 })
