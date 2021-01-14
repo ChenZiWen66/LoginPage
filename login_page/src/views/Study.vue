6139229817c52a55c9b15e779f54128d0141df84
chenziwen <401030526@qq.com>
2021-01-14 22:52:56 +0800

    1.新增了一个页面，第一次接触到了setup()函数

---
+++ login_page/src/views/Study.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    这是一个测试界面
+<!--    {{letter}}-->
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Study'
+  // 新的option,所有组件API函数都在此使用，只在初始化时执行一次
+  // setup()是组合API中第一个要使用的函数
+  // setup () {
+  //   const letter = 10
+  //   return {
+  //     letter
+  //   }
+  // }
+})
+</script>
+
+<style scoped>
+
+</style>
